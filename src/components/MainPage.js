58dadee69893e12fa30634131dbb67bc93dfea92
EllissaPeterson <ellissa@iastate.edu>
2019-11-02 10:13:07 -0500

    somthing

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CHeader from './CustomHeader';
-import { Button } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class MainPage extends React.Component {
 
@@ -8,6 +8,26 @@ export default class MainPage extends React.Component {
         return (
             <header className="App-header">
                 <CHeader/>
+                <Form>
+                    <FormGroup>
+                        <Label for="sortBy">Sort By:</Label>
+                        <Input type="select" name="sortBy" id="sortBy">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                        <Label for="filterBy">Filter By:</Label>
+                        <Input type="select" name="filterBy" id="filterBy">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                    </FormGroup>
+                </Form>
                 <Button color="warning" size="lg" style={{width: '75%'}} href="/roomPostFormPage" block>Make Post</Button>
                 <Button color="warning" size="lg" style={{width: '75%'}} href="/roomRequestFormPage" block>Make Request</Button>
             </header>
--- src/components/RoomPostFormPage.js
+++ src/components/RoomPostFormPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CHeader from './CustomHeader';
-import { Button } from 'reactstrap';
+import { Button,Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class RoomPostFormPage extends React.Component {
 
@@ -8,7 +8,12 @@ export default class RoomPostFormPage extends React.Component {
         return (
             <header className="App-header">
                 <CHeader/>
-                
+                <Form>
+                    <FormGroup>
+                        <Label for="postName">Name of Post:</Label>
+                        <Input type="text" name="postName" id="postName" placeholder="Post Name" />
+                    </FormGroup>
+                </Form>
             </header>
         )
     }
