adb60392e3d1be191db9d040a8e6418e61c90770
Andrew Yunt <andrew.yunt@gmail.com>
2019-11-02 01:52:49 -0500

    Update gitignore and fixes

---
+++ build/precache-manifest.03a0fe0cebe281cfaed504a402088c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81791ff8dc9a534cd8440804131a958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b22208b1bef626cc2d1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "51e8765d2c0d22b8169d",
+    "url": "/static/css/main.e624b174.chunk.css"
+  },
+  {
+    "revision": "2b22208b1bef626cc2d1",
+    "url": "/static/js/2.84de7337.chunk.js"
+  },
+  {
+    "revision": "51e8765d2c0d22b8169d",
+    "url": "/static/js/main.28c60b9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
