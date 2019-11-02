8f9c91b5c09e9310a56fc51eb168df87c51d5d7f
EllissaPeterson <ellissa@iastate.edu>
2019-11-02 16:57:00 -0500

    Update AccountPage.js

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -3,8 +3,18 @@ import CHeader from './CustomHeader';
 import { Form, FormGroup, Label, Input, Container, Row, Col, Button } from 'reactstrap';
 
 export default class MainPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          name: "name",
+          location: "Ames",
+          email: "ellissa@iastate.edu",
+          phoneNumber: "+15157201611"
+        };
+      }
     save (){
-        
+        //TODO
+        //update states using databases
     }
 
     render() {
@@ -21,19 +31,19 @@ export default class MainPage extends React.Component {
                             <Form>
                                 <FormGroup>
                                     <Label for="name">Name</Label>
-                                    <Input type="text" name="name" id="name" placeholder="Current name" />
+                                    <Input type="text" name="name" id="name" placeholder={this.state.name} />
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="location">Location</Label>
-                                    <Input type="text" name="location" id="location" placeholder="Current location" />
+                                    <Input type="text" name="location" id="location" placeholder={this.state.location} />
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="email">Email</Label>
-                                    <Input type="email" name="email" id="email" placeholder="Current email" />
+                                    <Input type="email" name="email" id="email" placeholder={this.state.email} />
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="phoneNumber">Phone Number</Label>
-                                    <Input type="text" name="phoneNumber" id="phoneNumber" placeholder="Current phone number" />
+                                    <Input type="text" name="phoneNumber" id="phoneNumber" placeholder={this.state.phoneNumber} />
                                 </FormGroup>
                                 <Button onClick={this.save}>Save</Button>
                             </Form>
